a3e0b3de2cddb25f906e63291781b191dbccd59e
CrowOnslaught <Crowblackclaws@gmail.com>
2020-11-11 14:49:31 +0100

    [master] Implementing Iteration2 (2)

--- workspace/projects/blog/src/app/comments-component/comments-component.component.ts
+++ workspace/projects/blog/src/app/comments-component/comments-component.component.ts
@@ -5,11 +5,34 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './comments-component.component.html',
   styleUrls: ['./comments-component.component.scss']
 })
+
+
 export class CommentsComponentComponent implements OnInit {
-
+  
   constructor() { }
+  
+  m_lastUsername:string;
+  m_lastDescription:string;
 
-  ngOnInit(): void {
+  m_comments = 
+  [
+    {name:"JohnMcClane covid21 closebeta", date:"11/11/2020 08:42",text:"tampoco penseis que es lo que dicen, nadie más ha podido hacer una prueba real en pacientes con esa vacuna, luego puede ser que sea un truño"},
+    {name:"Andavirus", date:"11/11/2020 01:28",text:"La vacuna es viagra molida. No le digas a nadie."},
+    {name:"LocoDelColaCao", date:"10/11/2020 22:14",text:"no mientas, no se ha acabado. Se ha acabado el currar en gayumbos sin que te despidan que es distinto"},
+    {name:"Bruce Lee ", date:"10/11/2020 21:37",text:"He salido tan poco de casa que no he necesitado darle la vuelta a los calzoncillos que empecé a utilizar en marzo."},
+  ];
+
+  onFormSubmit():void
+  {
+    // let l_result = 
+    // {
+    //   name: document.getElementById
+    // }
+    console.log(this.m_lastUsername + "|" + this.m_lastDescription);
+  }
+
+  ngOnInit(): void 
+  {  
   }
 
 }
